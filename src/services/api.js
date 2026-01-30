221267870ee96b3ac1fd5c90384971451374030a
neha-patankar <nehapatankar122@gmail.com>
2026-01-30 17:05:03 +0530

    ulr

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  // baseURL: "http://localhost:5000/api", 
+   baseURL: "https://hrms-backend-0r30.onrender.com/"
 });
