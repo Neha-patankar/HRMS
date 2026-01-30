bb85f9434234beefdf69e021831e3c94211cd52f
neha-patankar <nehapatankar122@gmail.com>
2026-01-30 17:29:10 +0530

    update code

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
+
 import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:5000/api", 
-   baseURL: "https://hrms-backend-0r30.onrender.com/api"
+  baseURL: "https://hrms-backend-0r30.onrender.com/api",
 });
+
