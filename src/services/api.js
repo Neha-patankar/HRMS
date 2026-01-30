ea9d2bb807295a6db8eefe110be4b884d7f47247
neha-patankar <nehapatankar122@gmail.com>
2026-01-30 17:35:51 +0530

    update code

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,17 @@
 
+// import axios from "axios";
+
+// export const api = axios.create({
+//   // baseURL: "http://localhost:5000/api", 
+//   baseURL: "https://hrms-backend-0r30.onrender.com/api",
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:5000/api", 
-  baseURL: "https://hrms-backend-0r30.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL, // Env variable
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
